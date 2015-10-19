86b7fc53557d23148d539cca3fbd34f0aadfb934
Pannawee <pw.lonx@gmail.com>
2015-10-19 13:12:07 +0700

    เริ่มใช้ GitHub

---
+++ app/src/main/java/baac/pannawee/baacrestaurant/MyOpenHelper.java
@@ -0,0 +1,21 @@
+package baac.pannawee.baacrestaurant;
+
+import android.content.Context;
+
+/**
+ * Created by BAAC on 10/19/15.
+ */
+public class MyOpenHelper {
+
+    //Explicit
+    private static final String DATABASE_NAME = "BAAC.db";
+
+    private static final int DATABASE_VERSION = 1;
+
+
+
+
+    public MyOpenHelper(Context context) {
+    } //Constructor
+
+}// This Main Class
